8cac83cbbac48cf1f85f543328928cfba7b1ec6d
Creighton Magoun <magoun@gmail.com>
2018-10-16 19:22:48 +0000

    Fix unfolding of FCC curriculum map before scraping html

--- hello.js
+++ hello.js
@@ -5,8 +5,24 @@ let getHTML = async (url) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
+  
+  // Need to click links to fully expand map
+  // Outer level uses superblock class
+  let superblock = await page.$("li[class='superblock ']");
+  while (superblock) {
+    await superblock.click();
+    superblock = await page.$("li[class='superblock ']");
+  }
+  
+  // Inner level uses block class
+  let block = await page.$("li[class='block ']");
+  while (block) {
+    await block.click();
+    block = await page.$("li[class='block ']");
+  }
+  
+  // Map is now fully expanded. Get content and close browser.
   const html = await page.content();
-
   await browser.close();
   return html;
 };
@@ -15,8 +31,8 @@ let getHTML = async (url) => {
 const url = 'https://learn.freecodecamp.org';
 getHTML(url).then( (html) => {
   const $ = require('cheerio').load(html);
-  $('li').each(function () {
-    console.log($(this).text());
+  $("li[class='map-challenge-title'] > a").each(function () {
+    // console.log($(this).text());
     });
 });
 

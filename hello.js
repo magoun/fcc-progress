a5790225cdd9f4468852564da0f6f364e3ae5d32
Creighton Magoun <magoun@gmail.com>
2018-10-17 15:16:21 +0000

    Refactor curriculum mapper into function call

--- hello.js
+++ hello.js
@@ -27,39 +27,78 @@ let getHTML = async (url) => {
   return html;
 };
 
-
-const url = 'https://learn.freecodecamp.org';
-getHTML(url).then( (html) => {
+let createMap = (html) => {
   const $ = require('cheerio').load(html);
-  let jsonArray = [];
+  let jsonObj = {};
   
   // Assemble the data to convert to JSON
   // Crawl the major sections in .superblock
   $('.superblock').each(function (index, element) {
     // Get the major section titles
     let section = $(this).find('h4').text();
-    jsonArray[index] = 
+    console.log(section);
+    
+    let sectionObj = {};
     // Crawl the subsections in .block
     $(this).find('.block')
       .each(function (index, element) {
         // Get the minor section titles
         let subsection = $(this).find('h5').text();
         console.log('  ' + subsection);
+        
+        let exercises = [];
+        // Get the individual exercises for the subsection
         $(this).find('a')
           .each(function (index, element) {
             // First item is an 'Intro item', which we can ignore
             if (index != 0) {
               let exercise = $(this).text();
+              exercises.push(exercise);
               console.log('    ' + exercise);
             }
           });
+        
+        // Store the exercise array as 'subsection' => exercises
+        sectionObj[subsection] = exercises;
       });
-    // console.log(testArray);
+      
+    // Store the section object as 
+    // 'section' => {subsection1 => exercises,
+    //               subsection2 => exercise, ...}
+    jsonObj[section] = sectionObj;
   });
-  // console.log(jsonArray);
   
-  // $("li[class='map-challenge-title'] > a").each(function () {
-  //   console.log($(this).text());
-  // });
-});
+  // console.log(JSON.stringify(jsonObj));
+  return JSON.stringify(jsonObj);
+}
+
+const url = 'https://learn.freecodecamp.org';
+getHTML(url).then(createMap);
+// getHTML(url).then( (html) => {
+//   const $ = require('cheerio').load(html);
+//   let jsonArray = [];
+  
+//   // Assemble the data to convert to JSON
+//   // Crawl the major sections in .superblock
+//   $('.superblock').each(function (index, element) {
+//     // Get the major section titles
+//     let section = $(this).find('h4').text();
+//     jsonArray[index] = 
+//     // Crawl the subsections in .block
+//     $(this).find('.block')
+//       .each(function (index, element) {
+//         // Get the minor section titles
+//         let subsection = $(this).find('h5').text();
+//         console.log('  ' + subsection);
+//         $(this).find('a')
+//           .each(function (index, element) {
+//             // First item is an 'Intro item', which we can ignore
+//             if (index != 0) {
+//               let exercise = $(this).text();
+//               console.log('    ' + exercise);
+//             }
+//           });
+//       });
+//   });
+// });
 

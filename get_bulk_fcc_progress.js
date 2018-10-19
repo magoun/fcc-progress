86eeb486645169d99c04e2a820ca5e8317e23f4b
Creighton Magoun <magoun@gmail.com>
2018-10-19 03:52:02 +0000

    Correct misnamed bulk/non-bulk files

--- get_bulk_fcc_progress.js
+++ get_bulk_fcc_progress.js
@@ -1,21 +1,28 @@
 /**
- * Scrapes a user profile from https://www.freecodecamp.org/{username} and 
- * compares completed challenges against the FCC curriculum. The results are
- * output in JSON format to outputFile.
+ * Scrapes multiple user profile from fcc_profiles.txt and 
+ * compares completed challenges against the Greenville Codes curriculum. 
  * 
  * Track Greenville Codes progress with gvl_codes_path.json.
- * Track overall FreeCodeCamp progress with fcc_path.json.
  * 
- * Usage: node fcc_progress.js
+ * Usage: node bulk_fcc_progress.js
  * 
  */
 
-const outputFile = 'fcc_progress.json',
-      url = 'https://www.freecodecamp.org/magoun',
+const outputFile = 'student_progress.csv',
+      urlFile = 'fcc_profiles.txt',
       trackFile = 'gvl_codes_path.json';
- 
+
 const fs = require('fs');
 const puppeteer = require('puppeteer');
+
+// Read list of profile urls
+// Returns an array of profile URLs
+let readURLs = (file) => {
+  let profiles = fs.readFileSync(file, 'utf8');
+  
+  // Splitting the return on new line yields an array
+  return profiles.split('\n');
+};
 
 // Fetch html using puppeteer
 let getHTML = async (url) => {
@@ -45,13 +52,15 @@ let parseHTML = (html) => {
 };
 
 // Compare progress to overall curriculum
-// Returns a JSON object
+// Returns a comma delimited string
 let getProgress = (completedChallenges) => {
   const rawJSON = fs.readFileSync(trackFile);
   let challengeMap = JSON.parse(rawJSON);
   
   // Inconsistent capitalization, thus map to lower case
   completedChallenges = completedChallenges.map(x => x.toLowerCase());
+
+  let progress = '';
 
   // Loop through curriculum JSON
   for (let section of Object.keys(challengeMap)) {
@@ -65,31 +74,41 @@ let getProgress = (completedChallenges) => {
         return completedChallenges.indexOf(val) != -1 ? ++acc : acc;
       }, 0);
       
-      challengeMap[section][subsection] = `${Math.round(100 * numCompleted / numChallenges)}%`;
+      progress += `${Math.round(100 * numCompleted / numChallenges)}%` + ',';
     }
   }
 
-  return challengeMap;
+  return progress;
 };
 
-// Writes the JSON object to a file
-let writeJSON = (json) => {
-  // Format the json for readability
-  const jsonString = JSON.stringify(json, null, 2);
+// Writes the url and progress strings to a file
+let writeProgress = (writeArray, file) => {
+  const outputString = writeArray.join('\n');
   
-  fs.writeFile(outputFile, jsonString, (err) => {
+  fs.writeFile(file, outputString, (err) => {
       // Report success / failure
-      const successMessage = 'Greenville Codes progress was written to ' + outputFile;
+      const successMessage = 'Greenville Codes progress was written to ' + file;
+        
       err ? console.log(err) : console.log(successMessage);
   }); 
 };
 
 // Runs the program
-let run = () => {
-  getHTML(url)
-    .then(parseHTML)
-    .then(getProgress)
-    .then(writeJSON);
+let run = async () => {
+  // Get urls from urlFile
+  const urlArray = readURLs(urlFile);
+  let writeArray = [];
+  
+  // Assemble student progress into array
+  for (let url of urlArray) {
+    let progress = await getHTML(url)
+                           .then(parseHTML)
+                           .then(getProgress);
+                       
+    writeArray[urlArray.indexOf(url)] = url + ',' + progress;
+  }
+  
+  writeProgress(writeArray, outputFile);
 };
 
 run();
